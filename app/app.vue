c82638a58102b35be9441512ac62fbb1a40d216a
Riverlance <biel94moraes@msn.com>
2025-11-24 18:57:22 -0300

    Margin to card info

--- app/app.vue
+++ app/app.vue
@@ -67,7 +67,7 @@
             <!-- cards go here -->
             <div class="bg-white rounded overflow-hidden shadow-md">
               <img src="/img/stew.jpg" alt="stew" />
-              <div>
+              <div class="m-4">
                 <span class="font-bold">5 Bean Chili Stew</span>
                 <span class="block text-gray-500 text-sm">Recipe by Mario</span>
               </div>
