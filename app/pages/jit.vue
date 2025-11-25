e980996c0ec67f30063f193125590bdadad2e2db
Riverlance <biel94moraes@msn.com>
2025-11-25 01:36:46 -0300

    More styling features of Tailwind CSS
    Important: JIT is the default behavior in recents versions, so you can ignore all subjects about JIT. The only thing I kept here, is about Tailwind CSS directives.

--- app/pages/jit.vue
+++ app/pages/jit.vue
@@ -1,23 +1,34 @@
 <template>
   <div>
 
-    <div>
-      <h1>Tailwind JIT Tutorial</h1>
-      <h2>(Just in Time)</h2>
+    <div class="p-8 text-center">
+      <h1 class="text-4xl text-blue-600 font-bold">Tailwind JIT Tutorial</h1>
+      <h2 class="text-2xl text-gray-500 font-semibold">(Just in Time)</h2>
     </div>
 
-    <div>
-      <button>Click me</button>
-      <button>Click me</button>
+    <div class="p-8">
+      <button class="bg-blue-600 text-white p-2 rounded">Click me</button>
+      <button class="text-white p-2 rounded">Click me</button>
     </div>
 
-    <div>
-      <h3>Todos:</h3>
-      <ul>
-        <li>Get out of bed</li>
-        <li>Play Mario Kart</li>
-        <li>Do some work</li>
+    <div class="p-8">
+      <h3 class="first-letter:font-bold first-letter:text-green-500">Todos:</h3>
+      <ul class="my-4 list-disc marker:text-blue-600 list-inside">
+        <li class="marker:text-green-500">Get out of bed</li>
+        <li class="selection:bg-pink-500">Play Mario Kart</li>
+        <li class="selection:bg-purple-500">Do some work</li>
       </ul>
+      <p class="first-line:font-semibold first-letter:text-6xl first-line:text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto quaerat laudantium aperiam non tempore rerum quia inventore praesentium quam officiis itaque, corporis pariatur quos quidem quibusdam deleniti quae quo dolores?</p>
+    </div>
+
+    <div class="p-8">
+      <button class="bg-[#f46225] text-[#fff6ca] p-2 rounded">click me</button>
+      <p class="text-[24px] mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium accusantium, eaque tempore sunt omnis ea, hic facere nulla vero, veniam dolor ad velit? Error, doloremque alias et temporibus maxime atque!</p>
+    </div>
+
+    <div class="p-8">
+      <button class="bg-[color:var(--primary)] text-white p-2 rounded">click me</button>
+      <p class="text-[length:var(--leadFont)]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae eius at nostrum assumenda totam illum quis culpa officia dicta voluptates. Obcaecati doloremque ipsum sunt nemo maxime, repellendus a iusto laboriosam.</p>
     </div>
 
   </div>
