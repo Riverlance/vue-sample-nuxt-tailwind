082a4cafefc65f998560975aec386bfcdfc6066a
Riverlance <biel94moraes@msn.com>
2025-11-25 01:35:52 -0300

    Add base page
    Important: JIT is the default behavior in recents versions, so you can ignore all subjects about JIT.

---
+++ app/pages/jit.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+
+    <div>
+      <h1>Tailwind JIT Tutorial</h1>
+      <h2>(Just in Time)</h2>
+    </div>
+
+    <div>
+      <button>Click me</button>
+      <button>Click me</button>
+    </div>
+
+    <div>
+      <h3>Todos:</h3>
+      <ul>
+        <li>Get out of bed</li>
+        <li>Play Mario Kart</li>
+        <li>Do some work</li>
+      </ul>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
